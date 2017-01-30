d0fbcaa75f9b9357b11631fc5f9fb33c1efb0522
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-01-30 16:02:08 -0500

    Calculator now adds 2 numbers and displays result.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
 var numberOne = document.getElementById("boxOne");
 var numberTwo = document.getElementById("boxTwo");
 var calcButton = document.getElementById("calculateButton");
+var javaanswer = document.getElementById("answer");
+var newtitle = document.getElementById("oldtitle");
+
 
 function add () {
-  var answer= (Number(numberOne.value) + Number(numberTwo.value));
-  console.log(answer);
+  var result= (Number(numberOne.value) + Number(numberTwo.value));
+  console.log(result);
+  javaanswer.innerHTML = result;
   }
 
 calcButton.addEventListener('click', add);
